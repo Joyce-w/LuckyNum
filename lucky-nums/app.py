0f3f1f26cd7b7fa0bad03e83c4c04f961245e8fd
Joyce <hellojoycewong@gmail.com>
2021-02-15 11:26:10 -0800

    post req sucess

--- lucky-nums/app.py
+++ lucky-nums/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+import requests
+import random
 from models import db, connect_db, LuckyInfo
 from flask_cors import CORS
 
+
 app = Flask(__name__)
+CORS(app)
 
 # specify that youre using postgres and a specific database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///lucky_num'
@@ -21,9 +25,21 @@ def homepage():
 
 @app.route("/api/get-lucky-num", methods=["POST"])
 def submit_data():
-    new_lucky = LuckyInfo(name='joyce', email='joyce@hotmail.com', year=1994, color='orange')
+
+    # get json data from axios 
+    name = request.get_json().get('name')
+    year = request.get_json().get('year')
+    email = request.get_json().get('email')
+    color = request.get_json().get('color')
+
+    # submit form data to db
+    new_lucky = LuckyInfo(name=name, year=year, email=email, color=color)
 
     db.session.add(new_lucky)
     db.session.commit()
+
+
+    # return post data in json
     res_json = jsonify(new=new_lucky.serialize())
     return (res_json, 201)
+
